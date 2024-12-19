baf193a35fda2fcc2f7b269972e74712b2b8fba7
haotian wang <1071921710@qq.com>
2024-12-20 03:01:16 +0800

    feat: add dds types

---
+++ teleop/simulation_node/dds_types.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from cyclonedds.idl import IdlStruct
+from cyclonedds.idl.types import array
+
+@dataclass
+class RotationMatrix(IdlStruct, typename="RotationMatrix"):
+    data: array[float, 9+16+16+12+12]  # SMPL rotation matrix
+
+@dataclass
+class Angle_38(IdlStruct, typename="angle38"):
+    data: array[float, 38]     # joint angles
+
+@dataclass
+class Angle_14(IdlStruct, typename="angle14"):
+    data: array[float, 38]
